c78c3320d9e5d67c3b8b6f11aaad81ae18b77fcb
Dotmeister01 <alongeadedotun@gmail.com>
2022-10-16 08:21:42 -0700

    0-positive_0r_negative.c

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,16 @@
+#include <studio.h>
+#include <stdlib.h>
+#include <time.h>
+/* */
+ 
+/*betty*/
+int main (void)
+{	
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your*/
+	return (0';
+}
+
