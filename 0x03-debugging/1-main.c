0cfd365dcd80d397a1777f3cb48f56680b9339fe
Dotmeister01 <alongeadedotun@gmail.com>
2022-10-23 11:12:53 -0700

    task 1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -5,6 +5,7 @@
 * Returin: 0
 */
 
+
 int main(void)
 {
 	int i;
@@ -15,7 +16,7 @@ int main(void)
 
 	/* while (i < 10)*/
 	/* {*/
-	/*	putchar(i);*/
+	/* putchar(i);*/
 	/* }*/
 
 	printf("Infinite loop avoided! \\o/\n");
