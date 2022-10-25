89a18e02f6d7a040ac1cc582ca9bb62ab51d983d
Dotmeister01 <alongeadedotun@gmail.com>
2022-10-25 06:41:38 -0700

    dont swap hordes in crossing a stream

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include main.h
+
+/**
+ * main - a function that swaps the values of a and b integers
+ * @a: int first
+ * @b: int second
+ * Dotmeister codes
+ * Return: 0
+ */
+
+void swap_int(int *a, int *b);
+{
+	int s;
+
+	s = *a;
+	*a = *b;
+	*b = s;
+}
